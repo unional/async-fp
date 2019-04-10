0b0b9faf8cc6e801bbafb5a709188d1ed141c7ae
Homa Wong (unional) <homawong@gmail.com>
2019-04-10 10:07:01 -0700

    chore: fix ci and clean up jest

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,1 @@
-const base = require('@unional/devpkg-node/simple/config/jest.common')
-
-module.exports = {
-  ...base,
-  name: 'io-fs',
-  displayName: 'io-fs',
-  roots: [
-    '<rootDir>/src'
-  ],
-  testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts', '**/*.node-spec.ts'],
-}
+module.exports = require('@unional/devpkg-node/simple/config/jest.common')
