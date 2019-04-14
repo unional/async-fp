c8cfc164926601172708f52b6dfd77e7b37c194b
Homa Wong (unional) <homawong@gmail.com>
2019-04-14 16:30:25 -0700

    fix: update to iso-error@2

--- src/errors.ts
+++ src/errors.ts
@@ -2,6 +2,6 @@ import { ModuleError } from 'iso-error';
 
 export class ContextNotSet extends ModuleError {
   constructor() {
-    super('async-fp', 0, 'set() is not called prior to get()')
+    super('async-fp', 'set() is not called prior to get()')
   }
 }
