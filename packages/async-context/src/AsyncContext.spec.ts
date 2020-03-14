f98a83d01ec3d43e88a943b3d0b86b07512999b0
Homa Wong <homawong@gmail.com>
2020-03-14 16:55:15 -0700

    feat(async-context): simplify API
    BREAKING CHANGE: API re-written

--- packages/async-context/src/AsyncContext.spec.ts
+++ packages/async-context/src/AsyncContext.spec.ts
@@ -1,175 +1,180 @@
 import a, { AssertOrder } from 'assertron'
-import { assertType } from 'type-plus'
-import { AsyncContext, ContextAlreadySet } from '.'
+import { assertType, assignability } from 'type-plus'
+import { AsyncContext } from './AsyncContext'
+import { ContextAlreadyInitialized } from './errors'
 
-test('create context with context object', async () => {
-  const ctx = new AsyncContext({ a: 1 })
+describe('constructor', () => {
+  test('with object', async () => {
+    const ctx = new AsyncContext({ a: 1 })
 
-  const { a } = await ctx.get()
+    const { a } = await ctx.get()
 
-  expect(a).toBe(1)
+    expect(a).toBe(1)
+  })
+
+  test('with Promise', async () => {
+    const ctx = new AsyncContext(Promise.resolve({ a: 1 }))
+
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('with initialize function', async () => {
+    const ctx = new AsyncContext(() => ({ a: 1 }))
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('with async initialize function', async () => {
+    const ctx = new AsyncContext(async () => ({ a: 1 }))
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('constructor initialize function is only called once', async () => {
+    const o = new AssertOrder(1)
+    const ctx = new AsyncContext(() => {
+      o.once(1)
+      return { a: 1 }
+    })
+    await ctx.get()
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+    o.end()
+  })
+
+  test('intialize function is not called until first get()', async () => {
+    new AsyncContext(() => { throw new Error('should not reach') })
+  })
 })
 
-test('create context with initialize function', async () => {
-  const ctx = new AsyncContext(() => ({ a: 1 }))
-  const { a } = await ctx.get()
+describe('initialize()', () => {
+  test('specify type through generics', async () => {
+    const ctx = new AsyncContext<{ a: string }>()
+    ctx.initialize({ a: 'a' })
+    const { a } = await ctx.get()
 
-  expect(a).toBe(1)
+    assertType.isTrue(assignability<string>()(a))
+    expect(a).toBe('a')
+  })
+
+  test('with object', async () => {
+    const ctx = new AsyncContext()
+
+    ctx.initialize({ a: 1 })
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('with promise', async () => {
+    const ctx = new AsyncContext()
+
+    ctx.initialize(Promise.resolve({ a: 1 }))
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('initialize function', async () => {
+    const ctx = new AsyncContext()
+
+    ctx.initialize(() => ({ a: 1 }))
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('with async initialize function', async () => {
+    const ctx = new AsyncContext()
+
+    ctx.initialize(async () => ({ a: 1 }))
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+  })
+
+  test('initialize function is only called once', async () => {
+    const o = new AssertOrder(1)
+    const ctx = new AsyncContext()
+    ctx.initialize(() => {
+      o.once(1)
+      return { a: 1 }
+    })
+    await ctx.get()
+    const { a } = await ctx.get()
+
+    expect(a).toBe(1)
+    o.end()
+  })
+
+  test('call initialize function with constructor initialized context throws ContextAlreadyInitialized', () => {
+    const ctx = new AsyncContext({ a: 1 })
+
+    a.throws(() => ctx.initialize({ a: 2 }), ContextAlreadyInitialized)
+  })
+
+  test('intialize function is not called until first get()', async () => {
+    const ctx = new AsyncContext()
+    ctx.initialize(() => { throw new Error('should not reach') })
+  })
 })
 
-test('create context with async initialize function', async () => {
-  const ctx = new AsyncContext(async () => ({ a: 1 }))
-  const { a } = await ctx.get()
+describe('extend()', () => {
+  test('with object', async () => {
+    const ctx = new AsyncContext({ a: 1 })
+    const actual = ctx.extend({ b: 'b' })
 
-  expect(a).toBe(1)
+    expect(await actual.get()).toEqual({ a: 1, b: 'b' })
+  })
+
+  test('with function', async () => {
+    const ctx = new AsyncContext({ a: 1 })
+    const actual = ctx.extend(() => ({ b: 'b' }))
+
+    expect(await actual.get()).toEqual({ a: 1, b: 'b' })
+  })
+
+  test('with async function', async () => {
+    const ctx = new AsyncContext({ a: 1 })
+    const actual = ctx.extend(async () => ({ b: 'b' }))
+
+    expect(await actual.get()).toEqual({ a: 1, b: 'b' })
+  })
+
+  test('overrides existing value', async () => {
+    const ctx = new AsyncContext({ a: 1 }).extend(() => ({ a: 2 }))
+    expect(await ctx.get()).toEqual({ a: 2 })
+  })
+  test('change types of existing property', async () => {
+    type Orig = { type: 'a' | 'b', value: string }
+    const ctx = new AsyncContext<Orig>({ type: 'a', value: '1' })
+      .extend(() => ({ value: 1 }))
+
+    const result = await ctx.get()
+
+    assertType<'a' | 'b'>(result.type)
+    assertType.isNumber(result.value)
+  })
+
+  test('handler receives current context for more better merge function reuse', async () => {
+    type Orig = { type: 'a' | 'b', value: number }
+
+    const orig = new AsyncContext<Orig>({ type: 'a', value: 1 })
+    const transform = async (context: AsyncContext<Orig>) => ({ value: String(await (await context.get()).value) })
+
+    const merged = orig.extend(transform)
+    expect(await merged.get()).toEqual({ type: 'a', value: '1' })
+  })
 })
 
-test('set context', async () => {
-  const ctx = new AsyncContext<{ a: number }>()
-
-  ctx.set({ a: 1 })
-
-  expect(await ctx.get()).toEqual({ a: 1 })
-})
-
-test('set context with function', async () => {
-  const ctx = new AsyncContext<{ a: number }>()
-
-  ctx.set(() => ({ a: 1 }))
-
-  expect(await ctx.get()).toEqual({ a: 1 })
-})
-
-test('set context with async function', async () => {
-  const ctx = new AsyncContext<{ a: number }>()
-
-  ctx.set(async () => ({ a: 1 }))
-
-  expect(await ctx.get()).toEqual({ a: 1 })
-})
-
-test('merge context', async () => {
-  const ctx = new AsyncContext({ a: 1 })
-  const actual = ctx.merge({ b: 'b' })
-
-  expect(await actual.get()).toEqual({ a: 1, b: 'b' })
-})
-
-test('merge context with function', async () => {
-  const ctx = new AsyncContext({ a: 1 })
-  const actual = ctx.merge(() => ({ b: 'b' }))
-
-  expect(await actual.get()).toEqual({ a: 1, b: 'b' })
-})
-
-test('merge context with async function', async () => {
-  const ctx = new AsyncContext({ a: 1 })
-  const actual = ctx.merge(async () => ({ b: 'b' }))
-
-  expect(await actual.get()).toEqual({ a: 1, b: 'b' })
-})
-
-test('get() waits for set() with no initial context', async () => {
+test('get() waits for initialize() with no initial context', async () => {
   const ctx = new AsyncContext()
-  setImmediate(() => ctx.set({ a: 2 }))
+  setImmediate(() => ctx.initialize({ a: 2 }))
   const { a } = await ctx.get()
   expect(a).toBe(2)
-})
-
-test('define context shape in type param', async () => {
-  const ctx = new AsyncContext<{ a: 1 }>()
-  ctx.set({ a: 1 })
-
-  const value = await ctx.get()
-  expect(value.a).toBe(1)
-})
-
-
-test('already set context cannot be set again', async () => {
-  a.throws(() => new AsyncContext({}).set({}), ContextAlreadySet)
-  a.throws(() => new AsyncContext(() => Promise.resolve({})).set({}), ContextAlreadySet)
-  a.throws(() => {
-    const ctx = new AsyncContext()
-    ctx.set({})
-    ctx.set({})
-  }, ContextAlreadySet)
-})
-
-test('lazy will resolve when invoking get()', async () => {
-  const o = new AssertOrder()
-  const ctx = new AsyncContext(() => {
-    o.once(2)
-    return Promise.resolve({ a: 1 })
-  }, { lazy: true })
-  o.once(1)
-  const actual = await ctx.get()
-  expect(actual).toEqual({ a: 1 })
-})
-
-test('lazy will not resolve context twice', async () => {
-  const o = new AssertOrder()
-  const ctx = new AsyncContext(() => {
-    o.once(2)
-    return Promise.resolve({ a: 1 })
-  }, { lazy: true })
-  o.once(1)
-  await ctx.get()
-  const actual = await ctx.get()
-  expect(actual).toEqual({ a: 1 })
-})
-
-test('lazy merge', async () => {
-  const o = new AssertOrder()
-  const ctx = new AsyncContext({ a: 1 })
-
-  const ctx2 = ctx.merge(() => {
-    o.once(2)
-    return Promise.resolve({ b: 2 })
-  }, { lazy: true })
-  o.once(1)
-  const actual = await ctx2.get()
-  expect(actual).toEqual({ a: 1, b: 2 })
-})
-
-test('merge lazy context', async () => {
-  const ctx = new AsyncContext(() => ({ a: 1 }), { lazy: true })
-    .merge({ b: 2 })
-
-  expect(await ctx.get()).toEqual({ a: 1, b: 2 })
-})
-
-test('merge overrides existing value', async () => {
-  const ctx = new AsyncContext({ a: 1 }).merge(() => ({ a: 2 }))
-  expect(await ctx.get()).toEqual({ a: 2 })
-})
-
-test('merge change types of existing property', async () => {
-  type Orig = { type: 'a' | 'b', value: string }
-  const ctx = new AsyncContext<Orig>({ type: 'a', value: '1' })
-    .merge(() => ({ value: 1 }))
-
-  const result = await ctx.get()
-
-  assertType<'a' | 'b'>(result.type)
-  assertType.isNumber(result.value)
-})
-
-test('merge function receives current context for more better merge function reuse', async () => {
-  type Orig = { type: 'a' | 'b', value: number }
-
-  const orig = new AsyncContext<Orig>({ type: 'a', value: 1 })
-  const transform = async (context: AsyncContext<Orig>) => ({ value: String(await (await context.get()).value) })
-
-  const merged = orig.merge(transform)
-  expect(await merged.get()).toEqual({ type: 'a', value: '1' })
-})
-
-test('transform creates a new context', async () => {
-  const ctx = new AsyncContext({ a: 1 })
-  const newCtx = ctx.transform(async original => {
-    expect(original).toBe(ctx)
-    const value = await original.get()
-    return { b: value.a + 1 }
-  })
-  expect(await newCtx.get()).toEqual({ b: 2 })
 })
