1e2d8abc04a8ec9dd7304e39af52945dcedd1c67
unional <homawong@gmail.com>
2023-04-08 12:54:45 -0700

    refactor: rename fixture def to gizmo

--- packages/gizmo/ts/define.spec.ts
+++ packages/gizmo/ts/define.spec.ts
@@ -1,75 +1,75 @@
 import { testType } from 'type-plus'
 import type {
-	AbstractDynamicDef,
-	AbstractOptionalDynamicDef,
-	AbstractOptionalRequireDef,
-	AbstractOptionalRequireDynamicDef,
-	AbstractPlusOptionalDef,
-	AbstractRequireDef,
-	AbstractRequireDynamicDef,
-	AbstractRequireOptionalDef,
-	AbstractRequireOptionalDynamicDef,
-	AbstractRequiredDef,
-	DynamicDef,
-	LeafDef,
-	LeafDefFn,
-	LeafTupleDef,
-	LeafTupleDefFn,
-	LeafWithStartDef,
-	LeafWithStartDefFn,
-	OptionalDef,
-	OptionalDynamicDef,
-	OptionalRequireDef,
-	OptionalRequireDynamicDef,
-	RequireDef,
-	RequireDynamicDef,
-	RequireOptionalDef,
-	RequireOptionalDynamicDef
+	AbstractDynamicGizmo,
+	AbstractOptionalDynamicGizmo,
+	AbstractOptionalRequireGizmo,
+	AbstractOptionalRequireDynamicGizmo,
+	AbstractPlusOptionalGizmo,
+	AbstractRequireGizmo,
+	AbstractRequireDynamicGizmo,
+	AbstractRequireOptionalGizmo,
+	AbstractRequireOptionalDynamicGizmo,
+	AbstractRequiredGizmo,
+	DynamicGizmo,
+	LeafGizmo,
+	LeafGizmoFn,
+	LeafTupleGizmo,
+	LeafTupleGizmoFn,
+	LeafWithStartGizmo,
+	LeafWithStartGizmoFn,
+	OptionalGizmo,
+	OptionalDynamicGizmo,
+	OptionalRequireGizmo,
+	OptionalRequireDynamicGizmo,
+	RequireGizmo,
+	RequireDynamicGizmo,
+	RequireOptionalGizmo,
+	RequireOptionalDynamicGizmo
 } from './fixtures'
 
 it(`gets the type of the define result for leaf def`, () => {
-	testType.equal<{ leaf: { foo(): number } }, LeafDef>(true)
-	testType.equal<{ leaf_tuple: { foo(): number } }, LeafTupleDef>(true)
-	testType.equal<{ leaf_start: { foo(): number } }, LeafWithStartDef>(true)
-	testType.equal<{ leaf_fn: { foo(): number } }, LeafDefFn>(true)
-	testType.equal<{ leaf_tuple_fn: { foo(): number } }, LeafTupleDefFn>(true)
-	testType.equal<{ leaf_start_fn: { foo(): number } }, LeafWithStartDefFn>(true)
+	testType.equal<{ leaf: { foo(): number } }, LeafGizmo>(true)
+	testType.equal<{ leaf_tuple: { foo(): number } }, LeafTupleGizmo>(true)
+	testType.equal<{ leaf_start: { foo(): number } }, LeafWithStartGizmo>(true)
+	testType.equal<{ leaf_fn: { foo(): number } }, LeafGizmoFn>(true)
+	testType.equal<{ leaf_tuple_fn: { foo(): number } }, LeafTupleGizmoFn>(true)
+	testType.equal<{ leaf_start_fn: { foo(): number } }, LeafWithStartGizmoFn>(true)
 })
 
 it(`gets the type of the define result for static def`, () => {
-	testType.equal<{ abstract_required: { foo(): number } }, AbstractRequiredDef>(true)
-	testType.equal<{ require: { foo(): number } }, RequireDef>(true)
-	testType.equal<{ optional: { foo(): number } }, OptionalDef>(true)
+	testType.equal<{ abstract_required: { foo(): number } }, AbstractRequiredGizmo>(true)
+	testType.equal<{ require: { foo(): number } }, RequireGizmo>(true)
+	testType.equal<{ optional: { foo(): number } }, OptionalGizmo>(true)
 
-	testType.equal<{ abstract_require: { foo(): number } }, AbstractRequireDef>(true)
-	testType.equal<{ abstract_optional: { foo(): number } }, AbstractPlusOptionalDef>(true)
-	testType.equal<{ require_optional: { foo(): number } }, RequireOptionalDef>(true)
-	testType.equal<{ optional_require: { foo(): number } }, OptionalRequireDef>(true)
+	testType.equal<{ abstract_require: { foo(): number } }, AbstractRequireGizmo>(true)
+	testType.equal<{ abstract_optional: { foo(): number } }, AbstractPlusOptionalGizmo>(true)
+	testType.equal<{ require_optional: { foo(): number } }, RequireOptionalGizmo>(true)
+	testType.equal<{ optional_require: { foo(): number } }, OptionalRequireGizmo>(true)
 
-	testType.equal<{ abstract_require_optional: { foo(): number } }, AbstractRequireOptionalDef>(true)
-	testType.equal<{ abstract_optional_require: { foo(): number } }, AbstractOptionalRequireDef>(true)
+	testType.equal<{ abstract_require_optional: { foo(): number } }, AbstractRequireOptionalGizmo>(true)
+	testType.equal<{ abstract_optional_require: { foo(): number } }, AbstractOptionalRequireGizmo>(true)
 })
 
 it(`gets the type of the define result for dynamic def`, () => {
-	testType.equal<{ dynamic: { foo(): number } }, DynamicDef>(true)
+	testType.equal<{ dynamic: { foo(): number } }, DynamicGizmo>(true)
 })
 
 it(`gets the type of the define result for mix def`, () => {
-	testType.equal<{ abstract_dynamic: { foo(): number } }, AbstractDynamicDef>(true)
-	testType.equal<{ require_dynamic: { foo(): number } }, RequireDynamicDef>(true)
-	testType.equal<{ optional_dynamic: { foo(): number } }, OptionalDynamicDef>(true)
+	testType.equal<{ abstract_dynamic: { foo(): number } }, AbstractDynamicGizmo>(true)
+	testType.equal<{ require_dynamic: { foo(): number } }, RequireDynamicGizmo>(true)
+	testType.equal<{ optional_dynamic: { foo(): number } }, OptionalDynamicGizmo>(true)
 
-	testType.equal<{ abstract_require_dynamic: { foo(): number } }, AbstractRequireDynamicDef>(true)
-	testType.equal<{ abstract_optional_dynamic: { foo(): number } }, AbstractOptionalDynamicDef>(true)
-	testType.equal<{ require_optional_dynamic: { foo(): number } }, RequireOptionalDynamicDef>(true)
-	testType.equal<{ optional_require_dynamic: { foo(): number } }, OptionalRequireDynamicDef>(true)
+	testType.equal<{ abstract_require_dynamic: { foo(): number } }, AbstractRequireDynamicGizmo>(true)
+	testType.equal<{ abstract_optional_dynamic: { foo(): number } }, AbstractOptionalDynamicGizmo>(true)
+	testType.equal<{ require_optional_dynamic: { foo(): number } }, RequireOptionalDynamicGizmo>(true)
+	testType.equal<{ optional_require_dynamic: { foo(): number } }, OptionalRequireDynamicGizmo>(true)
 
 	testType.equal<
 		{ abstract_require_optional_dynamic: { foo(): number } },
-		AbstractRequireOptionalDynamicDef
+		AbstractRequireOptionalDynamicGizmo
 	>(true)
 	testType.equal<
 		{ abstract_optional_require_dynamic: { foo(): number } },
-		AbstractOptionalRequireDynamicDef
+		AbstractOptionalRequireDynamicGizmo
 	>(true)
 })
