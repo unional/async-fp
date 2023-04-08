6bfcc3a11f5b9cf6a734435862c3d9bd99b4263f
unional <homawong@gmail.com>
2023-04-08 13:01:14 -0700

    fix: clean up types
    remove Name, use unknown when possible

--- packages/gizmo/ts/define.ts
+++ packages/gizmo/ts/define.ts
@@ -4,27 +4,25 @@ import type { DepBuilder, Gizmo, InferAllGizmo } from './types'
  * Define a gizmo.
  */
 export function define<
-	Name extends string,
 	Static extends DepBuilder<unknown, unknown>,
 	Dynamic extends Record<string, DepBuilder<unknown, unknown>> | unknown,
 	Result extends
-		| [result: Record<string | symbol, any>, start?: () => Promise<any>]
+		| [result: Record<string | symbol, unknown>, start?: () => Promise<unknown>]
 		| Record<string | symbol, any>
 		| void
->(plugin: Gizmo<Name, Static, Dynamic, Result>): typeof plugin
+>(plugin: Gizmo<Static, Dynamic, Result>): typeof plugin
 /**
  * Define a gizmo function.
  */
 export function define<
-	Name extends string,
 	Static extends DepBuilder<unknown, unknown>,
 	Dynamic extends Record<string, DepBuilder<unknown, unknown>> | unknown,
-	Params extends any[],
+	Params extends unknown[],
 	Result extends
-		| [result: Record<string | symbol, any>, start?: () => Promise<any>]
+		| [result: Record<string | symbol, unknown>, start?: () => Promise<unknown>]
 		| Record<string | symbol, any>
 		| void
->(plugin: (...args: Params) => Gizmo<Name, Static, Dynamic, Result>): typeof plugin
+>(plugin: (...args: Params) => Gizmo<Static, Dynamic, Result>): typeof plugin
 export function define(plugin: unknown): typeof plugin {
 	return plugin
 }
