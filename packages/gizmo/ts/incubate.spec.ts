6ac00cf36fc6ab67b3cced3972b97265ce078837
unional <homawong@gmail.com>
2023-04-08 13:24:36 -0700

    test: incubate with gizmo function

--- packages/gizmo/ts/incubate.spec.ts
+++ packages/gizmo/ts/incubate.spec.ts
@@ -1,15 +1,20 @@
 import { testType } from 'type-plus'
-import { leafGizmo, leafTupleGizmo } from './fixtures'
+import { leafGizmo, leafGizmoFn, leafTupleGizmo } from './fixtures'
 import { incubate } from './incubate'
 
-it('incubate with an initial gizmo', async () => {
+it('incubates with an initial gizmo', async () => {
 	const r = await incubate(leafGizmo).create()
 
 	testType.equal<typeof r, { leaf: { foo(): number } }>(true)
 	expect(r.leaf.foo()).toEqual(1)
 })
 
-it('incubate with gizmos', async () => {
+it('does not have start function if no initial gizmo', () => {
+	const i = incubate()
+	testType.equal<keyof typeof i, 'with'>(true)
+})
+
+it('incubates using `with()`', async () => {
 	const r = await incubate().with(leafGizmo).with(leafTupleGizmo).create()
 
 	testType.equal<typeof r, { leaf: { foo(): number }; leaf_tuple: { foo(): number } }>(true)
@@ -17,7 +22,9 @@ it('incubate with gizmos', async () => {
 	expect(r.leaf_tuple.foo()).toEqual(1)
 })
 
-it('does not have start function before the first with() call', () => {
-	const i = incubate()
-	testType.equal<keyof typeof i, 'with'>(true)
+it('incubates with gizmo function', async () => {
+	const r = await incubate().with(leafGizmoFn(2)).create()
+	testType.equal<typeof r, { leaf_fn: { foo(): number } }>(true)
+
+	expect(r.leaf_fn.foo()).toEqual(2)
 })
