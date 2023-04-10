9abf3c147e291afadb98ff969854c5e6e3ac2cd6
unional <homawong@gmail.com>
2023-04-09 21:48:54 -0700

    fix: expose more types

---
+++ .changeset/small-guests-dress.md
@@ -0,0 +1,6 @@
+---
+'@unional/gizmo': patch
+---
+
+Export more types.
+To fix the type cannot be named issue.
--- packages/gizmo/ts/index.ts
+++ packages/gizmo/ts/index.ts
@@ -1,3 +1,11 @@
 export * from './define.js'
 export * from './incubate.js'
-export type { Gizmo, MissingDependency } from './types.js'
+export type {
+	Gizmo,
+	GizmoBase,
+	GizmoStatic,
+	GizmoDynamic,
+	GizmoBoth,
+	DepBuilder,
+	MissingDependency
+} from './types.js'
