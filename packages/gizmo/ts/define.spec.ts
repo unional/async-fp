6b76d1911e68af011d8208fb9c10af8be9623950
unional <homawong@gmail.com>
2023-04-22 14:41:38 -0700

    test: add test for explicit type
    for gizmo function with optional params

--- packages/gizmo/ts/define.spec.ts
+++ packages/gizmo/ts/define.spec.ts
@@ -1,5 +1,6 @@
 import { define } from './define.js'
 import { incubate } from './incubate.js'
+import { GizmoBase } from './index.js'
 
 it('can specify a sync start function', async () => {
 	const gizmo = define({
@@ -47,4 +48,15 @@ it('can specify a async start function', async () => {
 	const app = await incubate().with(gizmo).create()
 
 	expect(app.value.get()).toBe('started')
+})
+
+it('needs explicit type when defining a gizmo function with optional params', async () => {
+	// this is a bug in TypeScript introduced in 4.8, fixed in 5.1.0
+
+	const gizmoFn: (a?: number | undefined) => GizmoBase<void> = define((_a?: number) => ({
+		async create() {}
+	}))
+
+	await incubate().with(gizmoFn()).create()
+	await incubate().with(gizmoFn(1)).create()
 })
